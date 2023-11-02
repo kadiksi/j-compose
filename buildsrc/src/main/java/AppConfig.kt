4c0df89b9b849a73f97a22f94e36bb0b8261c656
Kadik <kadik@Kadiks.local>
2023-11-02 09:54:01 +0600

    Upgraded to R8

--- buildsrc/src/main/java/AppConfig.kt
+++ buildsrc/src/main/java/AppConfig.kt
@@ -4,8 +4,8 @@ object AppConfig {
     const val compileSdk = 31
     const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 12
-    const val versionName = "1.12"
+    const val versionCode = 13
+    const val versionName = "1.13"
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules =  "consumer-rules.pro"
     const val dimension = "default"
